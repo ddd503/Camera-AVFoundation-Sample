13cf355bf4ee60419ca8f1bc29df53c73669b501
ddd503 <skbsporta7@gmail.com>
2019-07-04 05:52:05 +0900

    add camera button

--- Camera-AVFoundation-Sample/Camera/Controller/CameraViewController.swift
+++ Camera-AVFoundation-Sample/Camera/Controller/CameraViewController.swift
@@ -10,7 +10,10 @@ import AVFoundation
 import UIKit
 
 final class CameraViewController: UIViewController {
+    @IBOutlet private weak var shutterButtonAreaView: UIView!
+    @IBOutlet private weak var shutterButtonView: UIView!
     @IBOutlet private weak var shutterButton: UIButton!
+    @IBOutlet private weak var captureSessionView: UIView!
 
     private let session: AVCaptureSession
     private let output: AVCapturePhotoOutput = .init()
@@ -33,21 +36,60 @@ final class CameraViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        captureSessionView.backgroundColor = .red
         if setupSession() {
             setupCamera()
         } else {
             print("outputの追加に失敗")
         }
-
-        // カメラの上にViewを乗せたい時はレイヤーを上にする
-        view.bringSubviewToFront(shutterButton)
+        setupShutterButton()
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         session.stopRunning()
+    }
+
+    // MARK: - Action
+
+    @IBAction func didTapShutterButton(_ sender: UIButton) {
+        output.capturePhoto(with: capturePhotoSettings, delegate: self)
+    }
+
+    @objc private func takePhoto(sender: UIButton) {
+        sender.isEnabled = false
+        output.capturePhoto(with: capturePhotoSettings, delegate: self)
+    }
+
+    @objc private func shrinkShutterButton(sender: UIButton) {
+        let animator = UIViewPropertyAnimator(duration: 0.2, curve: .easeOut, animations: { [weak self] in
+            self?.shutterButton.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        })
+        animator.startAnimation()
+    }
+
+    @objc private func restoreShutterButton(sender: UIButton) {
+        let animator = UIViewPropertyAnimator(duration: 0.1, curve: .easeOut, animations: { [weak self] in
+            self?.shutterButton.transform = .identity
+        })
+        animator.startAnimation()
+    }
+
+    // MARK: - Private
+
+    private func setupShutterButton() {
+        shutterButton.addTarget(self, action: #selector(shrinkShutterButton(sender:)), for: .touchDown)
+        shutterButton.addTarget(self, action: #selector(restoreShutterButton(sender:)), for: [.touchDragOutside, .touchUpInside])
+        shutterButton.addTarget(self, action: #selector(takePhoto(sender:)), for: .touchUpInside)
+        roundCorner(views: [shutterButtonView, shutterButton])
+        shutterButtonView.layer.borderColor = UIColor.white.cgColor
+        shutterButtonView.layer.borderWidth = 6
+        // カメラの上にViewを乗せたい時はレイヤーを上にする
+        view.bringSubviewToFront(shutterButtonAreaView)
     }
 
     private func setupSession() -> Bool {
@@ -69,18 +111,26 @@ final class CameraViewController: UIViewController {
         // カメラViewの向きを縦に固定
         layer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
         // layerの大きさを決める（画面の更新時には更新した方が良い）
-        layer.frame = view.frame
+        layer.frame = captureSessionView.frame
         // viewにlayerをadd
-        view.layer.addSublayer(layer)
+        captureSessionView.layer.addSublayer(layer)
     }
 
-    @IBAction func didTapShutterButton(_ sender: UIButton) {
-        output.capturePhoto(with: capturePhotoSettings, delegate: self)
+    private func roundCorner(views: [UIView]) {
+        view.layoutIfNeeded()
+        views.forEach {
+            $0.layer.masksToBounds = true
+            $0.layer.cornerRadius = $0.frame.width / 2
+        }
     }
+
 }
+
+// MARK: - AVCapturePhotoCaptureDelegate
 
 extension CameraViewController: AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        shutterButton.isEnabled = true
         print(output)
     }
 }
